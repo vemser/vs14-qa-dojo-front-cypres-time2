aa35ae8930c0b4e88c69cd3805b07955b8de1f62
Thales Brasileiro <thalesxbrasileiro@gmail.com>
2024-09-06 15:56:40 -0300

    feat: Adicionado o teste de deletar usuário - Thales brasileiro

--- cypress/e2e/adminTest.cy.js
+++ cypress/e2e/adminTest.cy.js
@@ -6,10 +6,28 @@ describe("Admin", () => {
   })
 
   it("BuscarUsuario", () => {
-
     cy.clicarBotaoAdmin()
     cy.preencherUserName()
     cy.clicarBtnSearch()
     cy.validacaoTextoBusca()
   });
+  
+  it("BuscarUsuarioInexistente", () => {
+    cy.clicarBotaoAdmin()
+    cy.preencherUserName()
+    cy.clicarBtnSearch()
+    cy.validacaoTextoUsuarioNaoEncontrado()
+  });
+  
+  it("DeletarUsuario", () => {
+    cy.clicarBotaoAdmin()
+    cy.clicarBtnDelecao()
+    cy.clicarBtnConfirmarDelecao()
+    cy.validacaoTextoDeletarUsuario()
+  });
+
+  
+
+
+
 });
--- cypress/support/pages/adminPage.js
+++ cypress/support/pages/adminPage.js
@@ -6,19 +6,46 @@ const campoUsername =
   "#app > div.oxd-layout.orangehrm-upgrade-layout > div.oxd-layout-container > div.oxd-layout-context > div > div.oxd-table-filter > div.oxd-table-filter-area > form > div.oxd-form-row > div > div:nth-child(1) > div > div:nth-child(2) > input";
 const btnSearch ='#app > div.oxd-layout.orangehrm-upgrade-layout > div.oxd-layout-container > div.oxd-layout-context > div > div.oxd-table-filter > div.oxd-table-filter-area > form > div.oxd-form-actions > button.oxd-button.oxd-button--medium.oxd-button--secondary.orangehrm-left-space'
 
+const btnDetelar = '#app > div.oxd-layout.orangehrm-upgrade-layout > div.oxd-layout-container > div.oxd-layout-context > div > div.orangehrm-paper-container > div.orangehrm-container > div > div.oxd-table-body > div:nth-child(2) > div > div:nth-child(6) > div > button:nth-child(1)'
+
+const btnConfirmarDelecao = '#app > div.oxd-overlay.oxd-overlay--flex.oxd-overlay--flex-centered > div > div > div > div.orangehrm-modal-footer > button.oxd-button.oxd-button--medium.oxd-button--label-danger.orangehrm-button-margin'
+
+const msgUsuarioInvalido = '#oxd-toaster_1';
+
+cons toster = '#oxd-toaster_1';
+
 Cypress.Commands.add("clicarBotaoAdmin", () => {
     cy.clickElement(btnAdmin);
 })
 
 Cypress.Commands.add("preencherUserName", () => {
     cy.sendText(campoUsername, "Admin");
+})
 
+Cypress.Commands.add("preencherUserNameInvalid", () => {
+    cy.sendText(campoUsername, "invalid");
 })
 
 Cypress.Commands.add("clicarBtnSearch", ()=>{
     cy.clickElement(btnSearch)
 })
 
+Cypress.Commands.add("clicarBtnDelecao", ()=>{
+    cy.clickElement(btnDetelar)
+})
+
+Cypress.Commands.add("clicarBtnConfirmarDelecao", ()=>{
+    cy.clickElement(btnConfirmarDelecao)
+})
+
 Cypress.Commands.add("validacaoTextoBusca",() =>{
     cy.validatedText(msgValidacaoSearch, "Admin")
+})
+
+Cypress.Commands.add("validacaoTextoUsuarioNaoEncontrado",() =>{
+    cy.validatedText(msgValidacaoSearch, "No Records Found")
+})
+
+Cypress.Commands.add("validacaoTextoDeletarUsuario",() =>{
+    cy.validatedText(msgValidacaoSearch, "")
 })
