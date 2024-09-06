89acddc0cca36f37c291fde75a6a75e8e9bd06a1
Carol Jacques <carolinajacques01@gmail.com>
2024-09-06 15:00:40 -0300

    carol

--- cypress/e2e/loginTest.cy.js
+++ cypress/e2e/loginTest.cy.js
@@ -1,16 +1,31 @@
 describe('LoginFeature', () => {
-  it('LoginComSucesso', () => {
+  it.only('LoginComSucesso', () => {
     cy.preencherCampoUsername("Admin")
     cy.preencherCampoPassword("admin123")
     cy.clicarBotaoLogin()
   })
 
-  it('LoginComSenhaInvalida', () => {
+  it.only('LoginComSenhaInvalida', () => {
     cy.preencherCampoUsername("Admin")
     cy.preencherCampoPassword("admin1234")
     cy.clicarBotaoLogin()
-    cy.validatedText()
+    cy.verifText("Invalid credentials")
 
   })
+
+  it.only('LoginComEmailInvalio', () => {
+    cy.preencherCampoUsername("Admin123")
+    cy.preencherCampoPassword("admin123")
+    cy.clicarBotaoLogin()
+    cy.verifText("Invalid credentials")
+  })
+
+  it.only('LoginComUsernameVazio', () => {
+    cy.preencherCampoPassword("admin123")
+    cy.clicarBotaoLogin()
+    cy.verifTextRequired("Required")
+  })
+
+
 
 })
--- cypress/support/pages/loginPage.js
+++ cypress/support/pages/loginPage.js
@@ -4,25 +4,27 @@ const campoPassword = 'form > div:nth-child(3) > div > div:nth-child(2) > input'
 
 const btnLogin = 'button[type="submit"]'
 
-const selector = ''
+const selector = '#app > div.orangehrm-login-layout > div > div.orangehrm-login-container > div > div.orangehrm-login-slot > div.orangehrm-login-form > div > div.oxd-alert.oxd-alert--error > div.oxd-alert-content.oxd-alert-content--error > p'
+
+const requiredUsername = ".oxd-input-group > .oxd-text"
 
 
-
-
-Cypress.Commands.add("preencherCampoUsername", (text)=>{
+Cypress.Commands.add("preencherCampoUsername", (text) => {
     cy.sendText(campoUsername, text)
 })
 
-Cypress.Commands.add("preencherCampoPassword", (text)=>{
-    cy.sendText(campoPassword,text)
+Cypress.Commands.add("preencherCampoPassword", (text) => {
+    cy.sendText(campoPassword, text)
 })
 
-Cypress.Commands.add("clicarBotaoLogin", ()=>{
+Cypress.Commands.add("clicarBotaoLogin", () => {
     cy.clickElement(btnLogin)
 })
 
-Cypress.Commands.add("", (text) => {
-    cy.validatedText(selector)
+Cypress.Commands.add("verifText", (text) => {
+    cy.validatedText(selector, text)
 })
 
-
+Cypress.Commands.add("verifTextRequired", (text) => {
+    cy.validatedText(requiredUsername, text)
+})
